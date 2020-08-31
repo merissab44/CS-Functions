63c60e24d9adb2ec2604264f645ce03e009d01d3
Merissa_Bridgeman <Merissa.bridgeman98@gmail.com>
2020-08-30 19:11:14 -0700

    changed math expression

--- Custom_Calculator.py
+++ Custom_Calculator.py
@@ -6,6 +6,6 @@ desired_profit = input(
 #type casts the input into a integer and converts that number (desired_profit into a decimal.
 profit_margin = int(desired_profit) / 100
 #calculates the price you must sell at in order to gain that profit margin.(net sales)
-sell_price = int(price) / (1 - profit_margin)
+sell_price = (profit_margin * int(price))+ int(price)
 #prints the result of the sell price to gain the profit margin you inputted.
 print(f"To get a profit margin of {desired_profit}%, you must sell your product for ${round(sell_price)}")
