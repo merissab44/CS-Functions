3aaeb629423e4b792d0c2504457efe78e7dace50
Merissa_Bridgeman <Merissa.bridgeman98@gmail.com>
2020-09-01 16:56:25 -0700

    First commit

--- Custom_Calculator.py
+++ Custom_Calculator.py
@@ -1,11 +1,16 @@
-#gathers the price of house or (Cost of goods).
-price = input("What is your total cost to produce your product?")
-#gathers the minimum profit margin you want to receive.
-desired_profit = input(
-    "what is the profit margin you would like to achieve?(enter whole number)")
-#type casts the input into a integer and converts that number (desired_profit into a decimal.
-profit_margin = int(desired_profit) / 100
-#calculates the price you must sell at in order to gain that profit margin.(net sales)
-sell_price = (profit_margin * int(price))+ int(price)
-#prints the result of the sell price to gain the profit margin you inputted.
-print(f"To get a profit margin of {desired_profit}%, you must sell your product for ${round(sell_price)}")
+while True:
+#gathers the cost to produce your product or (Cost of goods).
+        price = input("What is your total cost to produce your product? ")
+#gathers the markup profit margin you want to achieve.
+        desired_profit = input("what is the profit margin you would like to achieve?(enter whole number) ")
+        while int(desired_profit) < 0 or int(price) < 0:
+                print("Sorry, your response must not be negative.")
+                price = input("What is your total cost to produce your product?")
+                desired_profit = input("what is the profit margin you would like to achieve?(enter whole number)")
+#desired_profit was successfully parsed, meaning the value is a whole positive number.
+#type casts the input into a float and converts that number (desired_profit into a decimal.
+        profit_margin = int((desired_profit) / 100)
+#calculates the price you must sell at in order to gain that markup profit margin.(net sales)
+        sell_price = int((price)/(1- profit_margin))
+#prints the result of the sell price to gain the markup profit margin you inputted.
+        print(f"To get a profit margin of {desired_profit}%, you must sell your product for ${round(sell_price,2)}")
