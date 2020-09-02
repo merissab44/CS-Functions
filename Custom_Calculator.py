f6f11f2717c8a311f2cfab96e78df2f3fa211734
Merissa_Bridgeman <Merissa.bridgeman98@gmail.com>
2020-09-02 14:39:23 -0700

    changed

--- Custom_Calculator.py
+++ Custom_Calculator.py
@@ -1,18 +1,19 @@
 while True:
 #gathers the cost to produce your product or (Cost of goods).
         price = input("What is your total cost to produce your product? ")
+
+        if int(price) < 0:
+                print("sorry, the number can't be a negative")
+                continue
 #gathers the markup profit margin you want to achieve.
         desired_profit = input("what is the profit margin you would like to achieve?(enter whole number) ")
-        while int(desired_profit) < 0 or int(price) < 0:
-                print("Sorry, your response must not be negative.")
-                price = input("What is your total cost to produce your product?")
-                desired_profit = input("what is the profit margin you would like to achieve?(enter whole number)")
+        if int(desired_profit) < 0:
+                print("sorry, the number can't be a negative")
+                desired_profit = input("what is the profit margin you would like to achieve?(enter whole number) ")
                 continue
-        while int(desired_profit) > 100:
-                print("Sorry, I can't calculate that.")
-                price = input("What is your total cost to produce your product?")
-                desired_profit = input("what is the profit margin you would like to achieve?(enter whole number)")
-                break              
+        if int(desired_profit) > 100:
+                print("Sorry I can't calculate that. please enter a value between 0 and 100")
+                desired_profit = input("what is the profit margin you would like to achieve?(enter whole number) ")
 #desired_profit was successfully parsed, meaning the value is a whole positive number.
 #type casts the input into a float and converts that number (desired_profit into a decimal.
         profit_margin = int(desired_profit) / 100
