bf460a6c256004318f62905abbb812cd40f6cfbe
Merissa_Bridgeman <Merissa.bridgeman98@gmail.com>
2020-09-02 15:10:18 -0700

    changed file

--- Custom_Calculator.py
+++ Custom_Calculator.py
@@ -3,18 +3,19 @@ while True:
 #gathers the cost to produce your product or (Cost of goods).
         price = input("What is your total cost to produce your product? ")
 
-        if int(price) < 0:
+        while int(price) < 0:
                 print("sorry, the number can't be a negative")
+                price = input("What is your total cost to produce your product? ")
                 continue
 #gathers the markup profit margin you want to achieve. if it is negative, it will
 #notify you that a negative is not valid and to give another input.
         desired_profit = input("what is the profit margin you would like to achieve?(enter whole number) ")
-        if int(desired_profit) < 0:
+        while int(desired_profit) < 0:
                 print("sorry, the number can't be a negative")
                 desired_profit = input("what is the profit margin you would like to achieve?(enter whole number) ")
                 #this continue keyword, prompts python to move to the next iteration
                 continue
-        if int(desired_profit) > 100:
+        while int(desired_profit) > 100:
                 print("Sorry I can't calculate that. please enter a value between 0 and 100")
                 desired_profit = input("what is the profit margin you would like to achieve?(enter whole number) ")
 #desired_profit was successfully parsed, meaning the value is a whole positive number.
