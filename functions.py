746d3ec75fd70143da174fa1c4fce936bb003a60
Merissa_Bridgeman <Merissa.bridgeman98@gmail.com>
2020-09-02 18:52:47 -0700

    first commit

---
+++ functions.py
@@ -0,0 +1,22 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5,6)
+result = area(8,8)
+result = area(10,8)
+print(result)
+
+def subtract(num1,num2):
+    difference = num1 - num2
+    return difference
+difference = subtract(10,2)
+difference = subtract(6,2)
+print(difference)
+
+def add(num1, num2):
+    sum = num1 + num2
+    return sum
+sum = add(10,8)
+sum = add(200,100)
+print(sum)
