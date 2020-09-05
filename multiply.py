2d57725a38a633cd4fca104a3a449b81dce1e7d8
Merissa_Bridgeman <Merissa.bridgeman98@gmail.com>
2020-09-05 10:45:49 -0700

    final commit

---
+++ multiply.py
@@ -0,0 +1,9 @@
+#function that takes two parameters num1 and num2 and multiplies them together
+def multiply(num1,num2):
+    result = num1 * num2 
+    return result 
+
+result1 = multiply(5,8)
+print(result1)
+result2 = multiply(8,10)
+print(result2)
